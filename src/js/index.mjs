946a3721582420e7d35169fbf87a98410eef5a87
Jenfer93 <89216683+Jenfer93@users.noreply.github.com>
2022-11-24 10:57:45 +0100

    logout

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,9 +1,10 @@
 import * as listeners from "../js/handlers/index.mjs";
 
 const path = location.pathname;
+listeners.logoutListener();
 
 if (path === "/pages/auth/login.html") {
   listeners.loginUserForm(); 
 } else if (path === "/pages/auth/register.html") {
   listeners.registerUserForm(); 
-};
+} 
