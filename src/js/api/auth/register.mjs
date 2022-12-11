7bf6472e72977065fd4918348927879ebd7a338f
Jenfer93 <89216683+Jenfer93@users.noreply.github.com>
2022-12-11 14:18:20 +0100

    changes redirect after register

--- src/js/api/auth/register.mjs
+++ src/js/api/auth/register.mjs
@@ -22,5 +22,8 @@ export async function register(profile) {
   })
 
   const results = await response.json()  
-  return results  
+  
+    location.href="/pages/auth/login.html"
+    return results  
+ 
 };
